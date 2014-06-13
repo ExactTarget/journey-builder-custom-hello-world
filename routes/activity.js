d66603ce334d066d87a86e1e04e225c5f55cceb0
Benjamin Dean <benjamin@benjamindean.com>
2014-06-13 14:27:16 -0700

    Show me more on operating state POSTs

--- routes/activity.js
+++ routes/activity.js
@@ -25,6 +25,23 @@ function logData( req ) {
         secure: req.secure,
         originalUrl: req.originalUrl
     });
+        console.log( "body: " + req.body );
+        console.log( "headers: " + req.headers );
+        console.log( "trailers: " + req.trailers );
+        console.log( "method: " + req.method );
+        console.log( "url: " + req.url );
+        console.log( "params: " + req.params );
+        console.log( "query: " + req.query );
+        console.log( "route: " + req.route );
+        console.log( "cookies: " + req.cookies );
+        console.log( "ip: " + req.ip );
+        console.log( "path: " + req.path );
+        console.log( "host: " + req.host );
+        console.log( "fresh: " + req.fresh );
+        console.log( "stale: " + req.stale );
+        console.log( "protocol: " + req.protocol );
+        console.log( "secure: " + req.secure );
+        console.log( "originalUrl: " + req.originalUr );
 }
 
 /*
